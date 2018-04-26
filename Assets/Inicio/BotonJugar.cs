10f3a25a9a979da67337d83da0ff0dd79cf2852d
Felipe Valencia <pipevalencia0112@hotmail.com>
2018-04-25 23:46:52 -0500

    se agrega pantalla de inicio, aun incompleta, y se escalan tamaños de el nivel 1

---
+++ Assets/Inicio/BotonJugar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonJugar : MonoBehaviour {
+
+	
+	//void OnTriggerEnter (Collider other) {
+	//	if(other.tag == "BotonJugar")
+ //       {
+ //           SceneManager.LoadScene("Level1");
+ //       }
+	//}
+
+    public void cambiarEscena ()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
